e8719aae1a457cf5805dad81cdc888e1da644c51
Usman Khan <muhammadusmankhan307@gmail.com>
2019-05-27 09:49:00 +0500

    Complete the Client and Server with request and response to the server.

--- client.py
+++ client.py
@@ -1,33 +1,110 @@
-from socket import *
-import pickle
+import socket
+import select
+import errno
+import sys
 
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
 
-HEADERSIZE = 10
-serverName = "localhost"
-serverPort = 4444
-BUFFER_SIZE = 1024
+my_username = input("Username: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((serverName, serverPort))
+username = my_username.encode('UTF-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('UTF-8')
+client_socket.send(username_header+username)
 
 while True:
+    message = input(f"{my_username} > ")
+    if message:
+        message = message.encode('UTF-8')
+        message_header = f"{len(message): < {HEADER_LENGTH}}".encode('UTF-8')
+        client_socket.send(message_header + message)
 
-    full_msg = b''
-    new_msg = True
-    while True:
-        msg = s.recv(BUFFER_SIZE)
-        if new_msg:
-            print(f"new message length: {msg[:HEADERSIZE]}")
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("connection closed by the server")
+                sys.exit()
+            username_length = int(username_header.decode("UTF-8").strip())
+            username = client_socket.recv(username_length).decode("UTF-8")
+            message_header = client_socket.recv(message_header).decode("UTF-8")
+            message_length = int(message_header.decode("UTF-8").strip())
+            message = client_socket.recv(message_length).decode("UTF-8")
 
-        full_msg += msg
+            print(f"{username} > {message}")
 
-        if len(full_msg)-HEADERSIZE == msglen:
-            print("full nsg recieved")
-            print(full_msg[HEADERSIZE:])
-            d = pickle.loads(full_msg[HEADERSIZE:])
-            print(d)
-            new_msg = True
-            full_msg = b''
-    print(full_msg)
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading Error', str(e))
+            sys.exit()
+        continue
+
+
+    except Exception as e:
+        print('General Error', str(e))
+        sys.exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from socket import *
+# import pickle
+
+
+# HEADERSIZE = 10
+# serverName = "localhost"
+# serverPort = 4444
+# BUFFER_SIZE = 1024
+
+# s = socket(AF_INET, SOCK_STREAM)
+# s.connect((serverName, serverPort))
+
+# while True:
+
+#     full_msg = b''
+#     new_msg = True
+#     while True:
+#         msg = s.recv(BUFFER_SIZE)
+#         if new_msg:
+#             print(f"new message length: {msg[:HEADERSIZE]}")
+#             msglen = int(msg[:HEADERSIZE])
+#             new_msg = False
+
+#         full_msg += msg
+
+#         if len(full_msg)-HEADERSIZE == msglen:
+#             print("full nsg recieved")
+#             print(full_msg[HEADERSIZE:])
+#             d = pickle.loads(full_msg[HEADERSIZE:])
+#             print(d)
+#             new_msg = True
+#             full_msg = b''
+#     print(full_msg)
--- server.py
+++ server.py
@@ -1,29 +1,93 @@
-from socket import *
-import time
-import pickle
+import socket
+import select
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+def recieve_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+        else:
+            message_length = int(message_header.decode('utf-8').strip())
+            return {"header":message_header, "data":client_socket.recv(message_length)}
+    except:
+        return False
+
+while True:
+    read_sockets, _, expection_sockets = select.select(sockets_list, [], sockets_list)
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+            user = recieve_message(client_socket)
+            if user is False:
+                continue
+            sockets_list.append(client_socket)
+            clients[client_socket] = user
+            print(f"accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf_8')}")
+        else:
+            message = recieve_message(notified_socket)
+            if message is False:
+                print(f"closed connection {clients[notified_socket]['data'].decode('utf-8')}")
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+            user = clients[notified_socket]
+            print(f"recieved msg from {user['data'].decode('utf-8')}:{message['data'].decode('utf-8')}")
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+    for notified_socket in expection_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
 
 
 
 
-HEADERSIZE = 10
 
-serverName = "localhost"
-serverPort = 4444
-BUFFER_SIZE = 1024
 
-s = socket(AF_INET, SOCK_STREAM)
-s.bind((serverName, serverPort))
-s.listen(5)
 
-print("Server is ready to receive data...")
 
-while 1:
-        clientsocket, address = s.accept()
-        print(f"connection from {address} is established")
+# from socket import *
+# import time
+# import pickle
 
-        d = {1: "Hey", 2: "There"}
-        msg = pickle.dumps(d)
-        # print(msg)
-        msg = bytes(f'{len(msg):<{HEADERSIZE}}', "UTF-8") + msg
-        clientsocket.send(msg)
+
+
+
+# HEADERSIZE = 10
+
+# serverName = "localhost"
+# serverPort = 4444
+# BUFFER_SIZE = 1024
+
+# s = socket(AF_INET, SOCK_STREAM)
+# s.bind((serverName, serverPort))
+# s.listen(5)
+
+# print("Server is ready to receive data...")
+
+# while 1:
+#         clientsocket, address = s.accept()
+#         print(f"connection from {address} is established")
+
+#         d = {1: "Hey", 2: "There"}
+#         msg = pickle.dumps(d)
+#         # print(msg)
+#         msg = bytes(f'{len(msg):<{HEADERSIZE}}', "UTF-8") + msg
+#         clientsocket.send(msg)
 
